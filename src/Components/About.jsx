77d75f104083207570b09f6d1251e4c54b192bba
S.Ashwin <ashwinsriram11@gmail.com>
2023-07-01 12:08:41 +0530

    Added detailed recipe page and about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,42 @@
 function About() {
   return (
     <div>
-      <h1>Hello</h1>
+      <div className="mx-16 my-12">
+        <p className="text-center mb-4 text-teal-900 text-3xl font-sans font-semibold">
+          Welcome to our recipe website!
+        </p>
+        <p className="font-serif tracking-wide leading-8 text-xl my-4">
+          At our recipe website, we strive to provide you with a delightful
+          culinary experience. Whether you're an experienced chef or a
+          passionate home cook, our platform is designed to inspire and assist
+          you in creating delicious meals that satisfy your cravings. We have
+          curated a vast collection of recipes spanning various cuisines,
+          flavors, and dietary preferences. From mouthwatering appetizers to
+          delectable main courses and indulgent desserts, our recipe database
+          offers a diverse range of options to suit every palate. To power our
+          recipe website, we rely on the MealDB API. The MealDB API is a
+          comprehensive and reliable source of culinary data that provides us
+          with access to an extensive catalog of recipes, ingredients, and
+          measurements. This integration allows us to present you with accurate
+          and up-to-date information, ensuring that you have all the necessary
+          details to recreate your favorite dishes with ease.
+        </p>
+        <p className="font-serif tracking-wide leading-8 text-xl my-4">
+          With our user-friendly interface, you can search for recipes based on
+          specific ingredients, dietary restrictions, or even explore new
+          recipes for culinary adventures. Each recipe comes with detailed
+          instructions, ingredient lists, and often includes helpful tips and
+          tricks to enhance your cooking skills.
+        </p>
+        <p className="font-serif tracking-wide leading-8 text-xl my-4">
+          Whether you're looking to impress your guests with a gourmet feast,
+          prepare a quick and nutritious weeknight dinner, or simply explore new
+          flavors, our recipe website is here to guide you on your culinary
+          journey. We hope you enjoy using our recipe website and discover new
+          culinary delights that bring joy to your kitchen and dining table.
+          Happy cooking!
+        </p>
+      </div>
     </div>
   );
 }
--- src/Components/RecipeMain.jsx
+++ src/Components/RecipeMain.jsx
@@ -23,11 +23,146 @@ function RecipeMain() {
 
   return (
     <div>
-      <h1>{recipe.strMeal}</h1>
-      <h3>Cuisine : {recipe.strArea}</h3>
-      <p>Category : {recipe.strCategory}</p>
-      <img src={recipe.strMealThumb} alt={recipe.strMeal} />
-      <p>{recipe.strInstructions}</p>
+      <h1 className="pt-8 text-center text-3xl font-sans font-semibold text-teal-900">
+        {recipe.strMeal}
+      </h1>
+      <div className="m-5 p-5 font-serif grid grid-cols-2 grid-flow-row">
+        <div className="text-slate-500">
+          <p className="text-xl pl-8 py-5">
+            <span className="font-bold text-slate-950">Cuisine : </span>
+            {recipe.strArea}
+          </p>
+          <p className="text-xl pl-8 py-5">
+            <span className="font-bold text-slate-950">Category : </span>
+            {recipe.strCategory}
+          </p>
+          <div className="text-xl pl-8 py-5">
+            <p className="text-xl font-bold pb-4 text-slate-950">Ingredients</p>
+            <ol>
+              {recipe.strIngredient1 && (
+                <li>
+                  {recipe.strIngredient1} - {recipe.strMeasure1}
+                </li>
+              )}
+              {recipe.strIngredient2 && (
+                <li>
+                  {recipe.strIngredient2} - {recipe.strMeasure2}
+                </li>
+              )}
+              {recipe.strIngredient3 && (
+                <li>
+                  {recipe.strIngredient3} - {recipe.strMeasure3}
+                </li>
+              )}
+              {recipe.strIngredient4 && (
+                <li>
+                  {recipe.strIngredient4} - {recipe.strMeasure4}
+                </li>
+              )}
+              {recipe.strIngredient5 && (
+                <li>
+                  {recipe.strIngredient5} - {recipe.strMeasure5}
+                </li>
+              )}
+              {recipe.strIngredient6 && (
+                <li>
+                  {recipe.strIngredient6} - {recipe.strMeasure6}
+                </li>
+              )}
+              {recipe.strIngredient7 && (
+                <li>
+                  {recipe.strIngredient7} - {recipe.strMeasure7}
+                </li>
+              )}
+              {recipe.strIngredient8 && (
+                <li>
+                  {recipe.strIngredient8} - {recipe.strMeasure8}
+                </li>
+              )}
+              {recipe.strIngredient9 && (
+                <li>
+                  {recipe.strIngredient9} - {recipe.strMeasure9}
+                </li>
+              )}
+              {recipe.strIngredient10 && (
+                <li>
+                  {recipe.strIngredient10} - {recipe.strMeasure10}
+                </li>
+              )}
+              {recipe.strIngredient11 && (
+                <li>
+                  {recipe.strIngredient11} - {recipe.strMeasure11}
+                </li>
+              )}
+              {recipe.strIngredient12 && (
+                <li>
+                  {recipe.strIngredient12} - {recipe.strMeasure12}
+                </li>
+              )}
+              {recipe.strIngredient13 && (
+                <li>
+                  {recipe.strIngredient13} - {recipe.strMeasure13}
+                </li>
+              )}
+              {recipe.strIngredient14 && (
+                <li>
+                  {recipe.strIngredient14} - {recipe.strMeasure14}
+                </li>
+              )}
+              {recipe.strIngredient15 && (
+                <li>
+                  {recipe.strIngredient15} - {recipe.strMeasure15}
+                </li>
+              )}
+              {recipe.strIngredient16 && (
+                <li>
+                  {recipe.strIngredient16} - {recipe.strMeasure16}
+                </li>
+              )}
+              {recipe.strIngredient17 && (
+                <li>
+                  {recipe.strIngredient17} - {recipe.strMeasure17}
+                </li>
+              )}
+              {recipe.strIngredient18 && (
+                <li>
+                  {recipe.strIngredient18} - {recipe.strMeasure18}
+                </li>
+              )}
+              {recipe.strIngredient19 && (
+                <li>
+                  {recipe.strIngredient19} - {recipe.strMeasure19}
+                </li>
+              )}
+              {recipe.strIngredient20 && (
+                <li>
+                  {recipe.strIngredient20} - {recipe.strMeasure20}
+                </li>
+              )}
+            </ol>
+          </div>
+        </div>
+        <div>
+          <img
+            className="border-slate-500 mt-5 border-4 w-[70%] ml-24"
+            src={recipe.strMealThumb}
+            alt={recipe.strMeal}
+          />
+          <p className="ml-24 p-4 mr-20 mt-5 text-center font-semibold text-teal-900">
+            View full recipe on{" "}
+            <a
+              className="bg-red-500 font-sans p-2 text-slate-100 rounded-2xl transition ease-in-out duration-300 hover:bg-red-400"
+              href={recipe.strYoutube}
+            >
+              YouTube
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="m-5 p-5 text-xl pl-8 py-5">
+        <p className=" ml-4 text-xl font-bold font-serif text-slate-950">Instructions</p>
+        <p className="ml-4 text-slate-500">{recipe.strInstructions}</p>
+      </div>
     </div>
   );
 }
